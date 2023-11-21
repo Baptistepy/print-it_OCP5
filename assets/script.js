eb7f92b581b7d219c233e722b703389d7012c974
Baptiste Boucly <b.boucly@outlook.com>
2023-11-21 08:25:57 +0100

    added image loop and dots updating

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,8 @@ const slides = [
 	}
 ]
 
+
+
 const leftArrow     = document.querySelector(".arrow_left");
 const rightArrow    = document.querySelector(".arrow_right");
 const dotsContainer = document.querySelector(".dots");
@@ -30,17 +32,39 @@ const dotsContainer = document.querySelector(".dots");
 // ******************************* VARIABLES *******************************
 
 let index = 0;
+let currentSlide = 0;
+let allDots;
 
 // ******************************* FONCTIONS *******************************
 
 function goLeft() {
   index      = (index + slides.length - 1) % slides.length;
   console.log(index)
+  updateImage();
+  updateDot();
 }
 
 function goRight() {
   index      = (index + 1 ) % slides.length;
   console.log(index)
+  updateImage();
+  updateDot();
+}
+
+function updateDot() {
+  const dotElements = document.querySelectorAll(".dot");
+  
+  dotElements.forEach(dot => {
+    dot.classList.remove("dot_selected");
+  });
+  
+  dotElements[index].classList.add("dot_selected");
+}
+
+function updateImage() {
+  const imageElement = document.querySelector(".banner-img");
+  const image = slides[index].image;
+  imageElement.src = "./assets/images/slideshow/" + image;
 }
 
 function goToSlide(index) {
@@ -74,7 +98,7 @@ function setDots() {
 setArrows();
 setDots();
 
-const dots = document.querySelectorAll(".dot");
+
 
 
 
